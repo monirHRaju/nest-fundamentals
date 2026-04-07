b869b8b522b0f760180d8aafcb8bb0f9aeeb7265
monirHRaju <monirhraju@gmail.com>
2026-04-07 21:41:00 +0600

    user module, controller, service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SongsModule } from './songs/songs.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [SongsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
